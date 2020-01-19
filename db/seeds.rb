8a45f85738a8089576c21ae33a83e729a833a804
EricCRodriguez <ecrod12@gmail.com>
2020-01-19 11:20:47 -0600

    added link for cat

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ User.create!(first_name: "Eric", last_name: "R", email: "eric@email.com", passwo
 User.create!(first_name: "Peter", last_name: "Jang", email: "peter@email.com", password: "password")
 User.create!(first_name: "Diana", last_name: "K", email: "diana@email.com", password: "password")
 
-LostPet.create!(user_id: 1, address: "215 W Ohio", search_area: "5 miles", breed: "poodle", color: "brown", size: "sm", age: "7", gender: "m", behaviour_type: "friendly", name: "spike", picture: "")
+LostPet.create!(user_id: 1, address: "215 W Ohio", search_area: "5 miles", breed: "poodle", color: "brown", size: "sm", age: "7", gender: "m", behaviour_type: "friendly", name: "spike", picture: "https://www.goodhousekeeping.com/life/pets/g21525625/why-cats-are-best-pets/?slide=4")
 LostPet.create!(user_id: 2, address: "1 S State", search_area: "2 miles", breed: "pitbull", color: "black", size: "med", age: "4", gender: "f", behaviour_type: "aggressive", name: "poli", picture: "")
 LostPet.create!(user_id: 3, address: "700 N Michigan", search_area: "10 miles", breed: "husky", color: "grey", size: "lg", age: "8", gender: "m", behaviour_type: "shy", name: "ozzy", picture: "")
 
