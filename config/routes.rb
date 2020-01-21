4a95cc4e0b867ec3b765e68d64afd2ba21b2ca89
EricCRodriguez <ecrod12@gmail.com>
2020-01-20 23:04:34 -0600

    updated code

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     post "/users" => "users#create"
     post "/sessions" => "sessions#create"
+    get "/users/:id" => "users#show"
 
     get "/lost_pets" => "lost_pets#index"
     post "/lost_pets" => "lost_pets#create"
