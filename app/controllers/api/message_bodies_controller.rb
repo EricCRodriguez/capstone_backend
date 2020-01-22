da437867177efb8663a2ea33a29484e0709404fa
EricCRodriguez <ecrod12@gmail.com>
2020-01-21 21:34:31 -0600

    almost final touches

--- app/controllers/api/message_bodies_controller.rb
+++ app/controllers/api/message_bodies_controller.rb
@@ -1,7 +1,25 @@
 class Api::MessageBodiesController < ApplicationController
   def index
+    @message = MessageBody.all
+    render "index.json.jb"
   end
 
   def create
+    @message = MessageBody.new(
+      sender_id: current_user.id,
+      body: params[:body],
+      picture: params[:picture],
+      receiver_id: params[:receiver_id],
+    )
+    if @message.save
+      render "show.json.jb"
+    else
+      render json: { errors: @message.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    @message = MessageBody.find_by(id: params[:id])
+    render "show.json.jb"
   end
 end
